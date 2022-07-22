53a131c9dd9567d1f1b89c6f61bc718cef137045
Keith Baker <github@baker.af>
2022-07-22 13:31:52 -0400

    refactor to account for Item contract

--- contracts/ItemManager.sol
+++ contracts/ItemManager.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.15;
 
+import "./Item.sol";
+
 contract ItemManager {
     enum SupplyChainSteps {
         Created,
@@ -10,31 +12,41 @@ contract ItemManager {
     }
 
     struct S_Item {
+        Item _item;
         ItemManager.SupplyChainSteps _step;
         string _identifier;
-        uint256 _priceInWei;
     }
     mapping(uint256 => S_Item) public items;
     uint256 index;
 
-    event SupplyChainStep(uint256 _itemIndex, uint256 _step);
+    event SupplyChainStep(uint256 _itemIndex, uint256 _step, address _address);
 
     function createItem(string memory _identifier, uint256 _priceInWei) public {
-        items[index]._priceInWei = _priceInWei;
+        Item item = new Item(this, _priceInWei, index);
+        items[index]._item = item;
         items[index]._step = SupplyChainSteps.Created;
         items[index]._identifier = _identifier;
-        emit SupplyChainStep(index, uint256(items[index]._step));
+        emit SupplyChainStep(index, uint256(items[index]._step), address(item));
         index++;
     }
 
     function triggerPayment(uint256 _index) public payable {
-        require(items[_index]._priceInWei <= msg.value, "Not fully paid");
+        Item item = items[_index]._item;
+        require(
+            address(item) == msg.sender,
+            "Only items are allows to update themselves"
+        );
+        require(item.priceInWei() == msg.value, "Not fully paid yet");
         require(
             items[_index]._step == SupplyChainSteps.Created,
             "Item is further in the supply chain"
         );
         items[_index]._step = SupplyChainSteps.Paid;
-        emit SupplyChainStep(_index, uint256(items[_index]._step));
+        emit SupplyChainStep(
+            _index,
+            uint256(items[_index]._step),
+            address(item)
+        );
     }
 
     function triggerDelivery(uint256 _index) public {
@@ -43,6 +55,10 @@ contract ItemManager {
             "Item is further in the supply chain"
         );
         items[_index]._step = SupplyChainSteps.Delivered;
-        emit SupplyChainStep(_index, uint256(items[_index]._step));
+        emit SupplyChainStep(
+            _index,
+            uint256(items[_index]._step),
+            address(items[_index]._item)
+        );
     }
 }
